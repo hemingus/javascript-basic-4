b355b7adc952b162616c40c825f5b21d7af61f5f
Heming Hanevik <heming.hanevik@gmail.com>
2025-10-06 12:36:38 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+// Main js file
+
+const diceContainer = document.getElementById("dice-container");
+const rollDiceButton = document.getElementById("roll-dice-button");
+
+function rollDice(numDice) {
+    for (let i = 0; i < numDice; i++) {
+        const roll = Math.floor(Math.random() * 6) + 1
+        const newDice = document.createElement("div");
+        newDice.id = `dice-${i}`;
+        newDice.classList.add("dice", "roll");
+        createDiceDots(roll, newDice);
+        diceContainer.appendChild(newDice);
+    }
+    setTimeout(() => newDice.classList.remove("roll"), 300);
+} 
+
+// Function that creates dots on the dice depending on its value
+function createDiceDots(num, dice) {
+    const dotPositions = {
+        1: [5],
+        2: [1, 9],
+        3: [1, 5, 9],
+        4: [1, 3, 7, 9],
+        5: [1, 3, 5, 7, 9],
+        6: [1, 3, 4, 6, 7, 9],
+    };
+
+    dotPositions[num].forEach((pos) => {
+        const dot = document.createElement("div");
+        dot.classList.add("dot")
+        dot.style.gridArea = getGridArea(pos);
+        dice.appendChild(dot);
+    });
+}
+
+// Function that maps each dot position to grid area
+function getGridArea(pos) {
+    const row = Math.ceil(pos / 3);
+    const col = pos % 3 === 0 ? 3 : pos % 3;
+    return `${row} / ${col} / ${row + 1} / ${col + 1}`;
+}
+
+rollDiceButton.addEventListener("click", () => rollDice(5));
